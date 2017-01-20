dc991a6a1fdf38920c70b6efa98e87d6661ea04e
yohei.kanehara <yohei.kanehara@vodori.com>
2017-01-20 03:01:15 -0600

    feature/mock-data - Simplified api.js into single functionˆ

--- front-end/src/store/api.js
+++ front-end/src/store/api.js
@@ -1,31 +1,7 @@
-// this is aliased in webpack config based on server/client build
-import api from 'create-api'
-
-// warm the front page cache every 15 min
-// make sure to do this only once across all requests
-if (api.onServer && !api.warmCacheStarted) {
-  api.warmCacheStarted = true
-  warmCache()
-}
-
-function warmCache () {
-  fetchItems((api.cachedIds.top || []).slice(0, 30))
-  setTimeout(warmCache, 1000 * 60 * 15)
-}
-
-function fetch (child) {
-  const cache = api.cachedItems
-  if (cache && cache.has(child)) {
-    return Promise.resolve(cache.get(child))
-  } else {
+export function fetchLinks(activeType) {
     return new Promise((resolve, reject) => {
-      // api.child(child).once('value', snapshot => {
-      //   const val = snapshot.val()
-      //   // mark the timestamp when this item is cached
-      //   if (val) val.__lastUpdated = Date.now()
-      //   cache && cache.set(child, val)
-      //   resolve(val)
-      // }, reject)
+        // TODO: Make API call to HAPI server
+        // TODO: Use activeType to get different links list
 
         const val = [{
             "timestamp": "2017-01-20T01:53:54.706749+00:00",
@@ -39,42 +15,6 @@ function fetch (child) {
             }
         }]
         if (val) val.__lastUpdated = Date.now()
-        cache && cache.set(child, val)
         resolve(val)
     })
-  }
-}
-
-export function fetchIdsByType (type) {
-  return api.cachedIds && api.cachedIds[type]
-    ? Promise.resolve(api.cachedIds[type])
-    : fetch(`${type}stories`)
-}
-
-export function fetchItem (id) {
-  return fetch(`item/${id}`)
-}
-
-export function fetchItems (ids) {
-  return Promise.all(ids.map(id => fetchItem(id)))
-}
-
-export function fetchUser (id) {
-  return fetch(`user/${id}`)
-}
-
-export function watchList (type, cb) {
-  let first = true
-  const ref = api.child(`${type}stories`)
-  const handler = snapshot => {
-    if (first) {
-      first = false
-    } else {
-      cb(snapshot.val())
-    }
-  }
-  ref.on('value', handler)
-  return () => {
-    ref.off('value', handler)
-  }
 }
