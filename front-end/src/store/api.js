88dfe272024c56c9ba2c21b4429627118ffa7d16
yohei.kanehara <yohei.kanehara@vodori.com>
2017-01-20 04:21:12 -0600

    ˆ

--- front-end/src/store/api.js
+++ front-end/src/store/api.js
@@ -3,17 +3,24 @@ export function fetchLinks(activeType) {
         // TODO: Make API call to HAPI server
         // TODO: Use activeType to get different links list
 
-        const val = [{
+        const russia = {
             "timestamp": "2017-01-20T01:53:54.706749+00:00",
             "url": "https://www.washingtonpost.com/news/worldviews/wp/2017/01/19/facebook-temporarily-blocked-rt-and-moscow-isnt-happy/?utm_term=.40173b487f91",
             "description": "test",
             "title": "Russia stinks",
-            "image": "none",
+            "imageUrl": "http://vignette2.wikia.nocookie.net/rickandmorty/images/1/1e/Rick_and_morty_icon.png/revision/latest?cb=20150805041642",
             "from": {
                 "mention_name": "da BEN",
                 "name": "Ben"
             }
-        }]
+        }
+
+        const val = [];
+
+        for (let i = 0; i < 25; ++i) {
+            val.push(russia);
+        }
+
         if (val) val.__lastUpdated = Date.now()
         resolve(val)
     })
