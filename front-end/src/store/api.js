75d9d5fb5b76d8f34d9ac2863a2d2731d926f00e
Anthony Spatafora <anthony.spatafora@vodori.com>
2017-01-20 10:17:42 -0600

    data from hapi :)

--- front-end/src/store/api.js
+++ front-end/src/store/api.js
@@ -1,9 +1,11 @@
+import axios from 'axios'
+
 export function fetchLinks(activeType) {
     return new Promise((resolve, reject) => {
         // TODO: Make API call to HAPI server
         // TODO: Use activeType to get different links list
 
-        const russia = {
+        let russia = {
             "timestamp": "2017-01-20T01:53:54.706749+00:00",
             "url": "https://www.washingtonpost.com/news/worldviews/wp/2017/01/19/facebook-temporarily-blocked-rt-and-moscow-isnt-happy/?utm_term=.40173b487f91",
             "description": "test",
@@ -23,7 +25,13 @@ export function fetchLinks(activeType) {
             val.push(link);
         }
 
-        if (val) val.__lastUpdated = Date.now()
-        resolve(val)
+        axios
+            .get('http://localhost:3000/')
+            .then(({ data }) => {
+                if (data) data.__lastUpdated = Date.now()
+                resolve(data)
+            })
+            .catch((err) => console.log(err))
+
     })
 }
