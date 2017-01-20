3d41421c9593178c36af6c634808914620fc6522
yohei.kanehara <yohei.kanehara@vodori.com>
2017-01-20 13:50:11 -0600

    feature/poster-name - Added list view creation

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -13,9 +13,9 @@ export default new Router({
   routes: [
     { path: '/vodori-news/:page(\\d+)?', component: createListView('vodori-news') },
     { path: '/vodori-news', redirect: '/vodori-news/1'},
-    { path: '/all-the-people/:page(\\d+)?', component: ItemView },
+    { path: '/all-the-people/:page(\\d+)?', component: createListView('all-the-people') },
     { path: '/all-the-people', redirect: '/all-the-people/1'},
-    { path: '/dev-io/:page(\\d+)?', component: ItemView },
+    { path: '/dev-io/:page(\\d+)?', component: createListView('dev-io') },
     { path: '/dev-io', redirect: '/dev-io/1'},
     { path: '/', redirect: '/vodori-news/1' }
   ]
